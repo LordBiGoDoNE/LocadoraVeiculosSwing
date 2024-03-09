f3d59f82b66dc59a31b8e89c5cd422d073d21f3c
Rafael Vieira da Silva <rafael_workspace@outlook.com>
2024-03-09 09:31:15 -0300

    Adicionado Tema FlatLaf

--- app/src/main/java/locadoraveiculosswing/App.java
+++ app/src/main/java/locadoraveiculosswing/App.java
@@ -1,14 +1,23 @@
 package locadoraveiculosswing;
 
 import bancolib.SqlUtil;
+import com.formdev.flatlaf.FlatDarculaLaf;
+import com.formdev.flatlaf.FlatDarkLaf;
+import com.formdev.flatlaf.FlatLightLaf;
 import locadoraveiculosswing.gui.TelaPrincipalGUI;
 
 public class App {
     
-    public static SqlUtil sqlUtil = new SqlUtil("localhost", "18745", "locadora", "postgres", "VrPost@Server");
+    public static SqlUtil sqlUtil = new SqlUtil("localhost", "18745", "postgres", "postgres", "VrPost@Server");
     
     public static void main(String[] args) {
+        initAparencia();
+        
         TelaPrincipalGUI form = new TelaPrincipalGUI();
         form.setVisible(true);
+    }
+    
+    private static void initAparencia(){
+        FlatDarculaLaf.setup();
     }
 }
